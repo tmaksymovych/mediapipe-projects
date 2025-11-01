23e4f2e8a746bce8dfde603125887e76b48fbe2a
tmaksymovych <tmaksymovych@kse.org.ua>
2025-11-01 22:14:20 +0200

    Initial commit (clean history)

---
+++ moves_operations.py
@@ -0,0 +1,60 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+# from math import hypot
+import pyautogui
+
+
+pTime = 0
+cTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+finger_was_up = False
+click_count = 0
+last_event_time = 0
+DOUBLE_CLICK_WINDOW = 0.5
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img,draw=False)
+    if len(lmList) != 0:
+        finger_tip = lmList[8]
+        finger_pip = lmList[7]
+        
+        y_tip = finger_tip[2]
+        y_dip = finger_pip[2]
+
+        finger_is_up = (y_tip < y_dip)
+        
+        if finger_is_up == True and finger_was_up == False:
+            click_count += 1
+
+            if click_count == 2:
+                time_diff = time.time() - last_event_time
+                if time_diff < DOUBLE_CLICK_WINDOW:
+                    pyautogui.press("up")
+                    click_count = 0
+                else:
+                    click_count = 1
+                    last_event_time = time.time()
+            elif click_count == 1:
+                last_event_time = time.time()
+        if time.time() - last_event_time > DOUBLE_CLICK_WINDOW and click_count == 1:
+            pyautogui.press("down")
+            click_count = 0
+
+
+
+        finger_was_up = finger_is_up
+
+        
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255),3)
+
+    cv2.imshow("Image", img) 
+    cv2.waitKey(1)
---
+++ volume_test.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+while True:
+    pyautogui.press('down')
+
+
+
+
+
+
+
+
+
+
+
+
